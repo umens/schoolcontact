66203480d60f9533b89038ae6af8b48c55e22cab
Mensa Ulysse <ulysse.mensa@gmail.com>
2013-07-09 17:17:17 +0200

    permettre de fusionner plusieurs socialAccount et ainsi de se connecter avec plusieurs reseaux
    mise en place de la sécurité définitive pour la partie utilisateur
    suppression anciens fichiers plus utilisé

--- src/Schoolcontact/UserBundle/Security/User/Provider/FOSUBUserProvider.php
+++ src/Schoolcontact/UserBundle/Security/User/Provider/FOSUBUserProvider.php
@@ -23,7 +23,7 @@ class FOSUBUserProvider extends BaseClass
         $setter_token = $setter.'AccessToken';
  
         //we "disconnect" previously connected users
-        if (null !== $previousUser = $this->userManager->findUserBy(array($property => $username))) {
+        if (null !== $previousUser = $this->userManager->findUserBy(array('username' => $username))) {
             $previousUser->$setter_id(null);
             $previousUser->$setter_token(null);
             $this->userManager->updateUser($previousUser);
@@ -42,7 +42,7 @@ class FOSUBUserProvider extends BaseClass
     public function loadUserByOAuthUserResponse(UserResponseInterface $response)
     {
         $username = $response->getUsername();
-        $user = $this->userManager->findUserBy(array($this->getProperty($response) => $username));
+        $user = $this->userManager->findUserBy(array('username' => $response->getRealName()));
         //when the user is registrating
         if (null === $user) {
             $service = $response->getResourceOwner()->getName();
@@ -94,13 +94,57 @@ class FOSUBUserProvider extends BaseClass
         }
  
         //if user exists - go with the HWIOAuth way
-        $user = parent::loadUserByOAuthUserResponse($response);
+       
+        //$user = parent::loadUserByOAuthUserResponse($response);
  
-        $serviceName = $response->getResourceOwner()->getName();
-        $setter = 'set' . ucfirst($serviceName) . 'AccessToken';
- 
-        //update access token
-        $user->$setter($response->getAccessToken());
+        $service = $response->getResourceOwner()->getName();
+        $setter = 'set'.ucfirst($service);
+        $setter_id = $setter.'Id';
+        $setter_token = $setter.'AccessToken';
+        // update user here
+        
+        //I have set all requested data with the user's username
+        //modify here with relevant data
+
+        switch ($service) {
+            case 'facebook':
+                $fb = $user->getFacebookId();
+                if(!isset($fb) OR $fb == ''){
+                    $data = $response->getResponse();
+                    $user->setUsername($response->getRealName());
+                    $user->setEmail($response->getEmail());
+                    $user->setPicture($response->getProfilePicture());
+                    $user->setFirstname($data['first_name']);
+                    $user->setLastname($data['last_name']);
+                    $user->setGender($data['gender']);
+                    $user->setLocation($data['location']['name']);     
+                    $user->setBirthday($data['birthday']);  
+                }
+                break;
+
+            case 'twitter':
+                $twitter = $user->getTwitterId();
+                if(!isset($twitter) OR $twitter == ''){
+                    $data = $response->getResponse();
+                    $user->setUsername($response->getRealName());
+                    $user->setEmail($username);
+                    $user->setPicture($response->getProfilePicture());
+                    $name = split(' ', $response->getRealName());
+                    $user->setFirstname($name[0]);
+                    $user->setLastname($name[1]);
+                    $user->setLocation($data['location']);
+                }
+                break;
+                
+            default:
+                // do something smart !
+                
+                break;
+        }
+
+        $user->$setter_token($response->getAccessToken());
+
+        $this->userManager->updateUser($user);
  
         return $user;
     }
