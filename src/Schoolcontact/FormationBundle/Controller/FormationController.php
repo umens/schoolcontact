ad4821cfa9cd6e1e5134cc2796d0789f3a5c2cc9
Mensa Ulysse <ulysse.mensa@gmail.com>
2013-07-10 08:19:26 +0200

    commencement codage actions
    - liste des formations par domaine de compétence
    - liste des ecoles par domaine de compétence
    - voir le détail d'une formation

--- src/Schoolcontact/FormationBundle/Controller/FormationController.php
+++ src/Schoolcontact/FormationBundle/Controller/FormationController.php
@@ -4,43 +4,96 @@ namespace Schoolcontact\FormationBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Schoolcontact\FormationBundle\Form\AddEcoleType;
-use Schoolcontact\FormationBundle\Form\AddUsersType;
-use Schoolcontact\FormationBundle\Form\AddFormationType;
+
 use Schoolcontact\FormationBundle\Entity\Departement;
 use Schoolcontact\FormationBundle\Entity\Diplome;
 use Schoolcontact\FormationBundle\Entity\Ecole;
 use Schoolcontact\FormationBundle\Entity\Formation;
 use Schoolcontact\FormationBundle\Entity\Region;
 use Schoolcontact\FormationBundle\Entity\Ville;
+use Schoolcontact\UserBundle\Entity\Domaine;
 use Schoolcontact\UserBundle\Entity\User;
+
+use Schoolcontact\FormationBundle\Form\AddEcoleType;
+use Schoolcontact\FormationBundle\Form\AddUsersType;
+use Schoolcontact\FormationBundle\Form\AddFormationType;
 
 class FormationController extends Controller
 {
-    public function indexAction()
-    {
+    public function indexAction(){
+
+        
+
         return $this->render('SchoolcontactFormationBundle:Formation:index.html.twig');
+    
     }
+
+    public function listFormationDomaineAction(Domaine $domaine){
+
+        $repository = $this->getDoctrine()->getManager()->getRepository('SchoolcontactFormationBundle:Formation');
+
+        $formations = $repository->findBy(array('domaine' => $domaine));
+        
+        return $this->render('SchoolcontactFormationBundle:Formation:viewFormation.html.twig', array('formations' => $formations));
+    
+    }
+
+    public function listEcoleDomaineAction(Domaine $domaine){
+
+        $repository = $this->getDoctrine()->getManager()->getRepository('SchoolcontactFormationBundle:Formation');
+
+        $formations = $repository->findBy(array('domaine' => $domaine));
+        
+        return $this->render('SchoolcontactFormationBundle:Formation:listEcoleDomaine.html.twig', array('formation' => $formation));
+    
+    }
+
+    public function viewFormationAction($id){
+
+        $repository = $this->getDoctrine()->getManager()->getRepository('SchoolcontactFormationBundle:Formation');
+
+        $formation = $repository->findOneById($id);
+        
+        return $this->render('SchoolcontactFormationBundle:Formation:viewFormation.html.twig', array('formation' => $formation));
+    
+    }
+
+
+
+
+
+
+
+
+
+
+
+
     public function consulterAction()
     {
         return $this->render('SchoolcontactFormationBundle:Formation:consulter.html.twig');
     }
+
     public function messagerieAction()
     {
         return $this->render('SchoolcontactFormationBundle:Formation:messagerie.html.twig');
     }
+
     public function accueilAction()
     {
         return $this->render('SchoolcontactFormationBundle:Formation:accueil.html.twig');
     }
+
     public function gestionAction()
     {
         return $this->render('SchoolcontactFormationBundle:Formation:gestion.html.twig');
     }
+
     public function inscriptionEcoleAction()
     {
         return $this->render('SchoolcontactFormationBundle:Formation:inscriptionecole.html.twig');
     }
+
     public function gestionecoleAction(Request $request)
     {
         //création d'un nouveau post
@@ -61,6 +114,7 @@ class FormationController extends Controller
         }
         return $this->render('SchoolcontactFormationBundle:Formation:gestionecole.html.twig', array('form' => $form->createView()));
     }
+
     public function gestionusersAction()
     {
         //création d'un nouveau post
@@ -81,6 +135,7 @@ class FormationController extends Controller
         }
         return $this->render('SchoolcontactFormationBundle:Formation:gestionusers.html.twig', array('form' => $form->createView()));
     }
+
     public function gestionformationAction()
     {
         //création d'un nouveau post
@@ -101,10 +156,12 @@ class FormationController extends Controller
         }
         return $this->render('SchoolcontactFormationBundle:Formation:gestionformation.html.twig', array('form' => $form->createView()));
     }
+
     public function moncompteAction()
     {
         return $this->render('SchoolcontactFormationBundle:Formation:monCompte.html.twig');
     }
+
     public function contactAction()
     {
         return $this->render('SchoolcontactFormationBundle:Formation:contact.html.twig');
---
+++ src/Schoolcontact/FormationBundle/Entity/Domaine.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Schoolcontact\FormationBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Domaine
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Schoolcontact\FormationBundle\Entity\DomaineRepository")
+ */
+class Domaine
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Domaine
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
